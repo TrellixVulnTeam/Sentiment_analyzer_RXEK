9bec431a35b6f14e9c4fc5f72a2788c3020ac78d
19Jules94 <19jules94@gmail.com>
2021-06-02 11:01:21 +0200

    :bug: bug con el archivo JSON

--- UI/views/multi_file.py
+++ UI/views/multi_file.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import os 
+import os
 from views import text_analysis
+import pandas as pd
+import tools.extra_functions
 
-def upload_file(folderpath='/home/jules/Documentos/Personal/TFG/Data'):
+
+def upload_file(folderpath='/home/jules/Documentos/Personal/Sentiment_analyzer/Data'):
+    main_dict = {}
     st.title("Choose a file:")
     input_file = st.file_uploader("", type="csv")
     subir = st.button('Upload')
@@ -12,10 +16,25 @@ def upload_file(folderpath='/home/jules/Documentos/Personal/TFG/Data'):
         subir = False
     elif (input_file is not None and subir == True):
         arch = os.path.join(folderpath, input_file.name)
+        main_dict['path_upload_file'] = arch
+        tools.extra_functions.dump(main_dict)
         return arch
+
+
+def open_csv():
+    csv_path = tools.extra_functions.load('path_upload_file')
+
+    if(csv_path is not None):
+        df = pd.read_csv(csv_path)
+        st.write(df)
+        columns = df.columns
+        option = st.selectbox('How would you like to be contacted?', (columns))
+        st.write('You selected:', option)
+    else:
+        st.warning("Dont data")
 
 
 def write():
     upload_file()
+    open_csv()
     text_analysis.option()
-    
--- UI/views/super_su.py
+++ UI/views/super_su.py
@@ -1,41 +1,41 @@
+import json
+
 import marshal
-import streamlit as st 
-import tkinter as tk 
+import streamlit as st
+import tkinter as tk
 from tkinter import filedialog
-import os 
+import os
 import sys
 sys.path.append('../src')
-import training
-import json
-def dump(dict):
-   with open('/home/jules/Documentos/Personal/TFG/memory/path.json', 'w') as fp:
-    json.dump(dict, fp) 
 
-def entrenamiento(folderpath='/home/jules/Documentos/Personal/TFG/Data'):
-    
+import training
+import tools.extra_functions 
+
+
+def entrenamiento(folderpath='/home/jules/Documentos/Personal/Sentiment_analyzer/Data'):
+
+    serialized()
     st.markdown("## Select the training part of the DataFrame")
-    training_part = st.select_slider("",options=range(101))/100
-    
-    
+    training_part = st.select_slider("", options=range(101))/100
+
     st.markdown("## Select the CSV File")
 
     input_file = st.file_uploader("", type="csv")
     subir = st.button('Upload')
 
-    s = serialized()
+    
 
     if (subir == True and input_file is None):
         st.error("You must select a file ")
         subir = False
     elif (input_file is not None and subir == True):
         arch = os.path.join(folderpath, input_file.name)
-        
-        training.training_part_algorithm(arch,training_part)
-        
+
+        training.training_part_algorithm(arch, training_part)
 
 
 def serialized():
-    main_dict ={}
+    main_dict = {}
     # Set up tkinter
     root = tk.Tk()
     root.withdraw()
@@ -45,22 +45,22 @@ def serialized():
 
     st.title('Write the file name :')
     file_name = str(st.text_input(""))
-    file_name=file_name.replace(' ','_')
+    file_name = file_name.replace(' ', '_')
     # Folder picker button
     st.title('Folder Picker')
-    
-    clicked = st.button('Folder Picker')
 
+    clicked = st.button('Folder Picker')
 
     if (clicked and file_name is not None):
         dirname = filedialog.askdirectory(master=root)
-        full_path = st.text_input('Selected folder:',os.path.join(dirname,file_name) )
-        main_dict['full_path']=full_path
-        dump(main_dict)
+        full_path = st.text_input( 'Selected folder:', os.path.join(dirname, file_name))
+        main_dict['full_path'] = full_path
+        tools.extra_functions.dump(main_dict)
 
         return full_path
-    print(main_dict)
+  
+
 
 def write():
-
- entrenamiento()
+    
+    entrenamiento()
