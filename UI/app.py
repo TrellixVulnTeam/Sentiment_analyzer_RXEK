f1803486a1b24d743990e22d9bd0a4865d534608
19Jules94 <19jules94@gmail.com>
2021-06-11 12:59:47 +0200

    Muestra en div

--- UI/app.py
+++ UI/app.py
@@ -1,9 +1,13 @@
 from flask import Flask 
 from flask import render_template
-
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/",methods=["GET","POST"])
 def home():
-    return render_template('home.html')
+    txt_area =''
+    if(request.method=='POST'):
+        txt_area=request.form.get('text_area',"")
+        
+    return render_template('home.html',resultado = txt_area)
