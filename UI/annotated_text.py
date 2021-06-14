295654847d7268ae680a8174288ae63c0f4865cc
19Jules94 <19jules94@gmail.com>
2021-06-14 07:53:43 +0200

    creando DataFrame

--- UI/annotated_text.py
+++ UI/annotated_text.py
@@ -1,29 +1,47 @@
 import flask
 from flask import Markup
 import marshal
+import pandas as pd
+
 
 def annotated_text(*text):
-    
+
     return text
+
 
 def procesed_text(text):
     key_words = deserialize_file()
-    
-    text = text.split(' ')    
-    lista = []    
+
+    text = text.split(' ')
+    lista = []
     for i in text:
         if(i in key_words):
-            t = Markup('<span class="color">' +i +'</span>')            
-            lista.append(t)          
-        else:
-            t = Markup('<span class="no-color" >' +i +'</span>')            
+            t = Markup('<span class="color">' + i + '</span>')
             lista.append(t)
-    return lista 
-
+        else:
+            t = Markup('<span class="no-color" >' + i + '</span>')
+            lista.append(t)
+    return lista
 
 
 def deserialize_file():
-    fileIn = open("/home/jules/Documentos/Personal/TFG/Serialized/binary/Racism.dat", "br")
-    dataLoad = marshal.load(fileIn)  
+    fileIn = open(
+        "/home/jules/Documentos/Personal/Sentiment_analyzer/Serialized/binary/Racism.dat", "br")
+    dataLoad = marshal.load(fileIn)
     fileIn.close()
     return dataLoad
+
+
+def dataframe():
+    data = pd.DataFrame()
+
+    df = pd.read_csv(
+        '/home/jules/Documentos/Personal/Sentiment_analyzer/Data/twitter_sexism_parsed_dataset.csv')
+    for i in df.select_dtypes(include=[object]):
+
+        data = df[i].str.slice(0, 20)
+    print(data)
+    return data
+
+
+
--- UI/app.py
+++ UI/app.py
@@ -2,14 +2,26 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import annotated_text as at
+
+from flask import Markup
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def home():
     txt_area =''
     a = ''
+ 
     if(request.method=='POST'):
         txt_area=request.form.get('text_area',"")
         racismo = request.form.get('racism',"")
         a = at.procesed_text(txt_area)
+       
+        
     return render_template('home.html',resultado = a)
+
+@app.route("/file-analysis",methods=["GET","POST"])
+def file_analysis():
+    df = ''
+    df = at.dataframe()
+    
+    return render_template('f.html',respuesta = Markup(df))
